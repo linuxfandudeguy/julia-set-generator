002d40649d6ac126121e5feea58f42e270866b4d
lelbois <164905463+linuxfandudeguy@users.noreply.github.com>
2024-10-27 16:39:05 -0400

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
   import Fractal from './Fractal.svelte';
 </script>
 
-<main class="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white">
+<main class="flex flex-col items-center justify-center min-h-screen bg-white-900 text-white">
   <h1 class="text-4xl font-bold mb-4">Fractal Generator</h1>
   <Fractal />
 </main>
